550237a447ecfd949400e1606edc036278a0df2c
Idrak Mustahsin <playbox8g@gmail.com>
2023-11-04 14:52:44 -0400

    Added router

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Autocomplete from "react-google-autocomplete";
-import Navi from "../components/nav"
+import Navi from "./components/nav"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Navi></Navi>
-    {/* landing page */}
+      <Navi></Navi>
+      {/* landing page */}
     </>
   )
 }
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,11 +3,10 @@ import { useState } from 'react'
 // import viteLogo from '/vite.svg'
 // import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Autocomplete from "react-google-autocomplete";
 // import Container from 'react-bootstrap/Container';
- import Nav from 'react-bootstrap/Nav';
+import Nav from 'react-bootstrap/Nav';
 //  import Navbar from 'react-bootstrap/Navbar';
- import NavDropdown from 'react-bootstrap/NavDropdown';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
@@ -17,19 +16,19 @@ function Navi() {
   return (
     <>
       <Navbar className="bg-body-tertiary">
-      <Container>
-      <img src="../src/assets/react.svg" width="28" class="d-inline-block align-top" alt=""/>
-        <Navbar.Brand href="#home">Maple University</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-        <NavDropdown title="Login" id="basic-nav-dropdown">
+        <Container>
+          <img src="../src/assets/react.svg" width="28" class="d-inline-block align-top" alt="" />
+          <Navbar.Brand href="#home">Maple University</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <NavDropdown title="Login" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Student</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Professor</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Admin</NavDropdown.Item>
             </NavDropdown>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   )
 }
