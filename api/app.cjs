fcd0c31477d7b698fe1901405ab4498393c87022
Karanpreet Raja <86526687+KaranpreetRaja@users.noreply.github.com>
2023-11-04 17:15:15 -0400

    adds more endpoints

--- api/app.cjs
+++ api/app.cjs
@@ -43,7 +43,7 @@ Endpoint: add a course to a student's active courses
 app.put("/student_active/add/courses")
 JSON request: {studentId: "studentId", courseId: "courseId"}
 */
-app.put("/student_active/add/courses/:id", express.json(), async (req, res) => {
+app.put("/student_active/add/courses", express.json(), async (req, res) => {
   try {
     // Check if studentId is present in the request body
     if (!req.body) {
@@ -52,7 +52,7 @@ app.put("/student_active/add/courses/:id", express.json(), async (req, res) => {
         .send("Missing studentId, course, or grade in the request body");
     }
 
-    const studentId = req.params.id;
+    const studentId = req.body.studentId;
     const courseToAdd = req.body.courseToAdd;
 
     // Get the student's document from the Firestore database
@@ -103,17 +103,141 @@ app.delete("/student_active/:studentId/course/:courseId", async (req, res) => {
 
 /*
 Endpoint: get all pending students
-app.get("/student_pending")
+app.get("/get_all_pending_students")
 JSON response: [{studentId: "studentId", firstName: "firstName", lastName: "lastName"}, ...]
 */
+app.get("/get_all_pending_students", async (req, res) => {
+  try {
+    // Get all pending students from the Firestore database
+    const pendingStudentsRef = db.collection("student_pending");
+    const pendingStudentsSnapshot = await pendingStudentsRef.get();
+
+    // Create an array of pending students
+    const pendingStudents = [];
+    pendingStudentsSnapshot.forEach((doc) => {
+      pendingStudents.push(doc.data());
+    });
+
+    // Send a success response
+    res.status(200).json(pendingStudents);
+  } catch (error) {
+    // Send an error response
+    res.status(500).send(error.message);
+  }
+});
 
 /*
+Endpoint: get all active students
+app.get("/get_all_active_students")
+JSON response: [{studentId: "studentId", firstName: "firstName", lastName: "lastName"}, ...]
+*/
+app.get("/get_all_active_students", async (req, res) => {
+  try {
+    // Get all active students from the Firestore database
+    const activeStudentsRef = db.collection("student_active");
+    const activeStudentsSnapshot = await activeStudentsRef.get();
 
+    // Create an array of active students
+    const activeStudents = [];
+    activeStudentsSnapshot.forEach((doc) => {
+      activeStudents.push(doc.data());
+    });
+
+    // Send a success response
+    res.status(200).json(activeStudents);
+  } catch (error) {
+    // Send an error response
+    res.status(500).send(error.message);
+  }
+});
+
+/*
+Endpoint: upgrade a pending student to an active student
+app.post("/upgrade_student")
+JSON request: {studentId: "studentId"}
 */
 
-// app.get(): get all pending students
+app.post("/upgrade_student", express.json(), async (req, res) => {
+  try {
+    // Check if studentId is present in the request body
+    if (!req.body) {
+      return res.status(400).send("Missing the request body");
+    }
 
-// app.delete(): remove a pending student
+    const studentId = req.body.studentId;
+
+    // Get the student's document from the Firestore database
+    const studentDoc = db.collection("student_pending").doc(studentId);
+
+    // Check if the student document exists
+    const studentSnapshot = await studentDoc.get();
+    if (!studentSnapshot.exists) {
+      return res.status(404).send("Student not found");
+    }
+
+    // Get the student data
+    const studentData = studentSnapshot.data();
+
+    // Create a new student document in the active students collection
+    await db.collection("student_active").doc(studentId).set(studentData);
+
+    // Delete the student document from the pending students collection
+    await studentDoc.delete();
+
+    // Send a success response
+    res.status(200).send("Student upgraded successfully");
+  } catch (error) {
+    // Send an error response
+    res.status(500).send(error.message);
+  }
+});
+
+/*
+Endpoint: create a new course
+app.post("/create_course")
+JSON request:
+{
+  courseId: "courseId",
+  courseName: "courseName",
+  courseDescription: "courseDescription",
+  courseCredits: "courseCredits",
+}
+*/
+app.post("/create_course", express.json(), async (req, res) => {
+  try {
+    // Check if courseId, courseName, courseDescription, and courseCredits are present in the request body
+    if (
+      !req.body.courseId ||
+      !req.body.courseName ||
+      !req.body.courseDescription ||
+      !req.body.courseCredits
+    ) {
+      return res
+        .status(400)
+        .send(
+          "Missing courseId, courseName, courseDescription, or courseCredits in the request body"
+        );
+    }
+
+    const courseId = req.body.courseId;
+    const courseName = req.body.courseName;
+    const courseDescription = req.body.courseDescription;
+    const courseCredits = req.body.courseCredits;
+
+    // Create a new course document in the courses collection
+    await db.collection("courses").doc(courseId).set({
+      courseName: courseName,
+      courseDescription: courseDescription,
+      courseCredits: courseCredits,
+    });
+
+    // Send a success response
+    res.status(200).send("Course created successfully");
+  } catch (error) {
+    // Send an error response
+    res.status(500).send(error.message);
+  }
+});
 
 // app.post(pending_student:id): add a pending student to active students
 
